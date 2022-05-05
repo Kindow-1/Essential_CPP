b696476e571394b1901a2826e5df038a97ecc01c
home <2351258517@qq.com>
2022-05-05 21:51:49 +0800

    2022.5.5

---
+++ chapt1/1_02.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string user_name;
+    int user_val;
+    int num_tries = 0; //初始化,使用=运算符
+    int num_right(0);  //初始化,构造函数语法
+    double user_score = 0.0;
+    char user_more;
+    bool go_for_it = true;
+    const int max_tries = 3;
+
+    return 0;
+}
